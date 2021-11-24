da59f531961a16b3839f10010856e5c898850de9
Emily <emilybribragg@gmail.com>
2021-11-23 19:11:40 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+//create a new variable and get element
+const main=document.querySelector('#main')
+//remove element
+main.remove()
+
+//STEP 2: has a 'newHeader' variable that points to node 'h1#victory'
+const newHeader = document.createElement('h1') //creates newHeader variable
+
+//STEP 3: has a 'newHeader' variable that points to node 'h1#victory'
+newHeader.setAttribute("id", "victory")
+
+//STEP 4: has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.innerHTML = 'Emily is the champion'
+
+//Finally, append the changes you made
+document.body.append.newHeader
